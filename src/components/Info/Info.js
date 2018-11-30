ef5308ff2dfd4f710e20f72f173f1e3df7f1edd2
unknown <https://github.com/JhonatanRios>
2018-11-30 00:43:17 -0500

    probar popup

---
+++ src/components/Info/Info.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default ({ close }) => (
+  <div className="info">
+    <ul>
+      <li onClick={close}>Home</li>
+    </ul>
+  </div>
+);
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
 import { store } from '../../stores/store';
+import Popup from "reactjs-popup";
 
 import "./Home.scss"
-import {action} from "mobx";
+import BurgerIcon from "../../components/BurgerIcon/BurgerIcon.js";
+import Info from "../../components/Info/Info.js";
 
 interface Props {
 }
@@ -13,8 +15,16 @@ interface State {
     imageStyle: any;
 }
 
+const contentStyle = {
+    background: "rgba(255,255,255,0)",
+    width: "80%",
+    border: "none"
+};
+
 @observer export class Home extends React.Component<Props, State> {
+
     shadowStyle: any = null;
+
     constructor(props: Props) {
         super(props);
         this.state = {
@@ -34,29 +44,37 @@ interface State {
                 // @ts-ignore
                 filter: `blur(${Math.abs(accelerationIncludingGravity.x)}px)`
             };
-            this.setState({accelerationIncludingGravityOH: accelerationIncludingGravity, imageStyle: _imageStyle})
+            this.setState({ accelerationIncludingGravityOH: accelerationIncludingGravity, imageStyle: _imageStyle })
         }
     };
 
     render() {
-
-
         return (
             <section className="homePadre">
                 <section className="landing">
-                    <div className="cont-logo">
-                        <img src={store.logo} alt="" className="logo"/>
+                    <div className="cont-logo" style={this.state.imageStyle}>
                     </div>
+                    <img src={store.logo} alt="" className="logo" />
                 </section>
                 <section className="aboutmeHome">
                     <div className="cont-description">
                         <div className="description">
                             <h1>Acerca de Mi</h1>
                             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem non unde facilis impedit vitae illo corrupti quam placeat eveniet. Inventore, atque aut. Odit illo recusandae sed rem deserunt? Harum, perferendis!</p>
+                            <div>
+                                <Popup
+                                    modal
+                                    overlayStyle={{ background: "rgba(255,255,255,0.98" }}
+                                    contentStyle={contentStyle}
+                                    closeOnDocumentClick={false}
+                                    trigger={open => <BurgerIcon open={open} />}>
+                                    {close => <Info close={close} />}
+                                </Popup>
+                            </div>
                         </div>
                     </div>
                     <div className="cont-myImage">
-                        <img src={store.men} alt="" className="men" style={this.state.imageStyle}/>
+                        <img src={store.men} alt="" className="men" />
                     </div>
                 </section>
                 <section className="viewProjects">
@@ -73,9 +91,14 @@ interface State {
                                         <img src={proy.img} alt="" />
                                         <h2>{proy.nombre}</h2>
                                         <h3>{proy.catego}</h3>
-                                        <button>
-
-                                        </button>
+                                        <Popup
+                                            modal
+                                            overlayStyle={{ background: "rgba(255,255,255,0.98" }}
+                                            contentStyle={contentStyle}
+                                            closeOnDocumentClick={false}
+                                            trigger={open => <BurgerIcon open={open} />}>
+                                            {close => <Info close={close} />}
+                                        </Popup>
                                     </div>
                                 )
                             })}
